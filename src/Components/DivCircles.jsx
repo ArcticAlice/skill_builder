c3b565f33e0e1d7073588b212dc75045747ba32d
ArcticAlice <156856574+ArcticAlice@users.noreply.github.com>
2025-01-07 19:12:30 -0500

    d

--- src/Components/DivCircles.jsx
+++ src/Components/DivCircles.jsx
@@ -1,18 +1,17 @@
 function DivCircles(props) {
     const divCircleStyles = {
         position: "absolute",
-        width: "10px",
-        height: "10px",
+        width: "13px",
+        height: "13px",
         zIndex: "3",
         backgroundColor: props.color,
         top: props.y,
         left: props.x,
         transform: `translate(-50%, -50%)`,
-        border: "1px solid #00B4D8",
         borderRadius: "50%"
     };
 
-    return <div style={divCircleStyles}></div>;
+    return <div className="divcircles" style={divCircleStyles}></div>;
 }
 
 export default DivCircles;
--- src/Pages/TaskPage.jsx
+++ src/Pages/TaskPage.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import DivCircles from "../Components/DivCircles";
 import Plus from "../assets/Plus";
-import X from "../assets/X";
 
 function PlusButton({ tap }) {
     const plusStyle = {
@@ -21,14 +20,51 @@ function PlusButton({ tap }) {
 
     return (
         <div style={plusStyle} onClick={tap}>
-            <Plus> style={{cursor: "pointer"}} </Plus>
+            <Plus> style={{ cursor: "pointer" }} </Plus>
         </div>
     );
 }
 
 function PopUp({ show, onClose, onSave }) {
+
     const [task, setTask] = useState("");
-    const [selectedColor, setSelectedColor] = useState("#FF0000"); // Default to the first color
+    const [selectedColor, setSelectedColor] = useState("#FF0000"); 
+
+    const [circleStyleEasy, setCircleStyleEasy] = useState({
+        position: "absolute",
+        display: "inline-block",
+        width: "10px",
+        height: "10px",
+        border: "1px solid #48CAE4",
+        borderRadius: "50%",
+        cursor: "pointer",
+        left: "5%",
+        top: "85%"
+    });
+
+    const [circleStyleMed, setCircleStyleMed] = useState({
+        position: "absolute",
+        display: "inline-block",
+        width: "10px",
+        height: "10px",
+        border: "1px solid #00B4D8",
+        borderRadius: "50%",
+        cursor: "pointer",
+        left: "15%",
+        top: "85%"
+    });
+
+    const [circleStyleHard, setCircleStyleHard] = useState({
+        position: "absolute",
+        display: "inline-block",
+        width: "10px",
+        height: "10px",
+        border: "1px solid #03045E",
+        borderRadius: "50%",
+        cursor: "pointer",
+        left: "25%",
+        top: "85%"
+    });
 
     if (!show) return null;
 
@@ -43,37 +79,55 @@ function PopUp({ show, onClose, onSave }) {
         onClose();
     };
 
+    const handleColorChange = (index, color) => {
+        
+        setCircleStyleEasy((c) => ({...c, backgroundColor: "none"}));
+        setCircleStyleMed((c) => ({...c, backgroundColor: "none"}));
+        setCircleStyleHard((c) => ({...c, backgroundColor: "none"}));
+
+        setSelectedColor(color);
+
+        if(index === 0) {
+            setCircleStyleEasy((c) => ({...c, backgroundColor: color}));
+        }else if(index === 1) {
+            setCircleStyleMed((c) => ({...c, backgroundColor: color}));
+        }else {
+            setCircleStyleHard((c) => ({...c, backgroundColor: color}));
+        }
+    };
+
     const popUpStyle = {
-        position: "absolute",
-        top: "50%",
-        left: "50%",
-        transform: "translate(-50%, -50%)",
-        backgroundColor: "#ADE8F4",
-        padding: "20px",
-        width: "300px",
-        height: "250px",
-        zIndex: "1000",
-        borderRadius: "8px",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
+        position: 'absolute',
+        width: '500px',
+        height: '150px',
+        backgroundColor: 'white',
+        padding: '20px',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 100,
+        overflow: 'auto',
+        borderRadius: '15px',
+        display: 'flex',
+        flexDirection: 'column',
+        boxSizing: 'border-box',
     };
 
     const closeButtonStyle = {
         position: "absolute",
-        top: "10px",
-        left: "10px",
+        top: "3%",
+        left: "1%",
         backgroundColor: "transparent",
         border: "none",
+        fontWeigth: "900",
         fontSize: "16px",
         cursor: "pointer",
     };
 
     const saveButtonStyle = {
         position: "absolute",
-        bottom: "10px",
-        right: "10px",
+        top: "76%",
+        left: "87%",
         backgroundColor: "#0077B6",
         color: "#FFF",
         border: "none",
@@ -84,66 +138,43 @@ function PopUp({ show, onClose, onSave }) {
 
     const textareaStyle = {
         flexGrow: 1,
-        resize: "none",
-        width: "100%",
-        height: "auto",
-        boxSizing: "border-box",
-        border: "1px solid #90E0EF",
-        borderRadius: "4px",
-        padding: "8px",
-        outline: "none",
-        color: "#03045E",
-        backgroundColor: "#E0F7FA",
-    };
-
-    const circlesContainerStyle = {
-        display: "flex",
-        justifyContent: "space-around",
+        resize: 'none',
+        width: '100%',
+        height: 'auto',
         marginTop: "10px",
+        marginBottom: '40px',
+        boxSizing: 'border-box',
+        border: 'none',
+        outline: 'none',
+        color: '#C8B6FF',
     };
 
-    const circleWrapperStyle = (color) => ({
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        width: "30px",
-        height: "30px",
-        borderRadius: "50%",
-        border: selectedColor === color ? "2px solid black" : "1px solid gray",
-        cursor: "pointer",
-    });
 
     return (
         <div style={popUpStyle}>
             <button style={closeButtonStyle} onClick={onClose}>
                 X
             </button>
+
             <textarea
                 style={textareaStyle}
                 placeholder="Enter Task!"
                 value={task}
                 onChange={(e) => setTask(e.target.value)}
             />
-            <div style={circlesContainerStyle}>
-                <div
-                    style={circleWrapperStyle("#FF0000")}
-                    onClick={() => setSelectedColor("#FF0000")}
-                >
-                    <DivCircles color="#FF0000" />
-                </div>
-                <div
-                    style={circleWrapperStyle("#00FF00")}
-                    onClick={() => setSelectedColor("#00FF00")}
-                >
-                    <DivCircles color="#00FF00" />
-                </div>
-                <div
-                    style={circleWrapperStyle("#0000FF")}
-                    onClick={() => setSelectedColor("#0000FF")}
-                >
-                    <DivCircles color="#0000FF" />
-                </div>
+
+            <div style={circleStyleEasy} onClick={() => handleColorChange(0, "#00B4D8")}>
+
             </div>
+
+            <div style={circleStyleMed} onClick={() => handleColorChange(1, "#00B4D8")}>
+
+            </div>
+
+            <div style={circleStyleHard} onClick={() => handleColorChange(2, "#03045E")}>
+
+            </div>
+
             <button style={saveButtonStyle} onClick={handleSave}>
                 Save
             </button>
@@ -152,26 +183,43 @@ function PopUp({ show, onClose, onSave }) {
 }
 
 
-function TaskTemplate({ taskName, taskColor }) {
-    const taskTemplateStyle = {
-        width: "80vh",
-        height: "10vh",
-        padding: "10px",
+function TaskTemplate({ taskName, taskColor, complete }) {
+
+    const completeButton = {
         border: "1px solid black",
-        marginBottom: "5px",
+        width: "15px",
+        height: "15px",
+        display: "inline-block"
+    }
+
+    const taskTemplateStyle = {
+        position: "relative",
+        width: "100vh",
+        height: "5vh",
+        padding: "5px",
+        paddingLeft: "10px",
+        border: "1px solid black",
+        marginBottom: "10px",
         borderRadius: "5px",
         display: "grid",
         gridTemplateColumns: "1fr 3fr",
         alignItems: "center",
         cursor: "pointer",
         userSelect: "none",
-        backgroundColor: "#F9F9F9",
+        backgroundColor: "#e6f9fc",
     };
+
+    const textStyle = {
+        verticalAlign: "center",
+        padding: "0",
+        margin: "0"
+    }
 
     return (
         <div style={taskTemplateStyle}>
-            <DivCircles color={taskColor} />
-            <h3>{taskName}</h3>
+            <div style={completeButton} onClick={complete}/>
+            <h3 style={textStyle}> {taskName} </h3>
+            <DivCircles x="95%" y="50%" color={taskColor} />
         </div>
     );
 }
@@ -184,18 +232,37 @@ function Tasks() {
         setTasks((prevTasks) => [...prevTasks, task]);
     };
 
+    const deleteTask = (task) => {
+        setTasks((t) => [...t.splice(task)]);
+    }
+
     const openPopUp = () => setIsPopUpOpen(true);
     const closePopUp = () => setIsPopUpOpen(false);
 
+    const taskPageStyle = {
+        padding: "0px",
+        margin: "0px",
+        width: "100vh",
+        height: "100vh",
+    }
+
+    const tasksStyle = {
+        position: "relative",
+        width: "60vh",
+        height: "95vh",
+        left: "30%",
+        top: "5%"
+    }
+
     return (
-        <div>
-            <h1>To-Do List</h1>
-            <div>
+        <div style={taskPageStyle}>
+            <div style={tasksStyle}>
                 {tasks.map((task, index) => (
                     <TaskTemplate
                         key={index}
                         taskName={task.taskName}
                         taskColor={task.taskColor}
+                        complete={deleteTask}
                     />
                 ))}
             </div>

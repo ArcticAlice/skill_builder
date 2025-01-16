3a26552dfd249dedf3ad21e9f82d0cbaf630a422
ArcticAlice <156856574+ArcticAlice@users.noreply.github.com>
2025-01-16 12:35:28 -0500

    Deploy React app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./skill_builder"
+  base: "./skill_builder",
+  plugins: [react()]
 })
